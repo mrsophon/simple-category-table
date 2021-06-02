803f78697ee8276a98244e2e927bf6dc15fb6434
Sophon <mrsophon@gmail.com>
2021-06-03 01:03:33 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      filterItems: [],
+      isLoaded: false,
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://api.publicapis.org/categories")
+      .then((res) => res.json())
+      .then((result) => {
+        this.setState({
+          isLoaded: true,
+          items: result,
+          filterItems: result,
+        });
+      });
+  }
+
+  filterList = (e) => {
+    const updatedList = this.state.items.filter((item) => {
+      return item.toLowerCase().search(e.target.value.toLowerCase()) !== -1;
+    });
+    this.setState({ filterItems: updatedList });
+  };
+
+  render() {
+    const { isLoaded, filterItems } = this.state;
+    const searchBox = <input type="text" onChange={this.filterList} />;
+
+    if (!isLoaded) {
+      return <div>Loading ... </div>;
+    } else {
+      return (
+        <div className="container">
+          <h1>Category</h1>
+
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            Filter Name: &nbsp;{searchBox}
+          </div>
+
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filterItems.map((item, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{item}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
